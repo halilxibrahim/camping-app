d6e9ada0d7c264ef00182de08098e70eb7adb984
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2023-09-28 11:53:02 +0300

    development-first

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {View, Image,Text, StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MapView from 'react-native-maps';
 
-export default function App() {
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+        <Image source={require('./assets/map.png')} style={styles.image} />
+      <Text>Home!</Text>
+    </View>  
+    );
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white', 
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function MapScreen() {
+  return(
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <View style={{ position: 'absolute', top: '50%', left: '50%', marginTop: -10, marginLeft: -10 }}>
+        <Text>Map!</Text>
+      </View>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
